f6838c5615df97bd83a498293017d68ce9ba5321
N <n@Ns-MacBook-Air.local>
2026-02-08 17:14:25 -0600

    update to main tf file

--- terraform/modules/ci-iam/main.tf
+++ terraform/modules/ci-iam/main.tf
@@ -48,6 +48,8 @@ data "aws_iam_policy_document" "s3_access" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
+      "s3:GetObjectTagging",
+      "s3:PutObjectTagging",
     ]
 
     resources = [
